a259e91252c4da560d17021b91006b6b2ea521e6
nwfrank <natefrank2048@gmail.com>
2025-06-07 15:42:38 -0500

    feature: set colors for navlinks

--- src/Components/Atoms/NavLink.jsx
+++ src/Components/Atoms/NavLink.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export const NavLink = (props) => {
   return (
-    <Link to={props.to} className="text-gray-700 hover:text-black">
+    <Link to={props.to} className="text-copy-secondary hover:text-blue-400">
       {props.children}
     </Link>
   );
